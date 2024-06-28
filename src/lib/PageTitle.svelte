54349e5d12011ddf22323e31f39dd53114f62515
Motocarota <motocarota@gmail.com>
2024-06-28 19:49:59 +0200

    adds page title

---
+++ src/lib/PageTitle.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title;
+</script>
+
+<h1
+	class="text-2xl text-center w-full font-[500] font-serif italic my-4 border-b border-dashedc capitalize"
+>
+	{title}
+</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import '../app.css';
+	import PageTitle from '../lib/PageTitle.svelte';
 	import Header from '../lib/Header.svelte';
 	import Footer from '../lib/Footer.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <Header />
-<div class="place-content-center text-center min-h-96 max-w-5xl mx-auto">
+
+<div class="text-center min-h-96 max-w-5xl mx-auto">
+	<PageTitle title={$page.url.pathname.substring(1)} />
 	<slot />
 </div>
 <Footer />

33faafcaba0285bf343312bf0af3a40acb46a583
Motocarota <motocarota@gmail.com>
2024-06-28 23:00:32 +0200

    replace require

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+import tailwindcss  from 'tailwindcss';
+import autoprefixer  from 'autoprefixer';
 
 export default {
   plugins: [
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 import flowbitePlugin from 'flowbite/plugin'
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {

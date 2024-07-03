078ccb4a4bbf646098e5d7b60ef58832aa4c1d2c
Motocarota <motocarota@gmail.com>
2024-07-03 11:56:10 +0200

    rm trailing slash from titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,18 @@
 	import Header from '../lib/Header.svelte';
 	import Footer from '../lib/Footer.svelte';
 	import { page } from '$app/stores';
+
+	function removeTrailingSlash(str) {
+		return str.replace(/\/+$/, '');
+	}
+
+	$: title = removeTrailingSlash($page.url.pathname.substring(1));
 </script>
 
 <Header />
 
 <div class="text-center min-h-96 max-w-5xl mx-auto">
-	<PageTitle title={$page.url.pathname.substring(1)} />
+	<PageTitle {title} />
 	<slot />
 </div>
 <Footer />

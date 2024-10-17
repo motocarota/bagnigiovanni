d2140c99c98da1951a4a127a65d4e3639037a2e3
Motocarota <motocarota@gmail.com>
2024-10-17 17:30:17 +0200

    adds second webcam

--- src/routes/webcam/+page.svelte
+++ src/routes/webcam/+page.svelte
@@ -3,8 +3,21 @@
 	height="600"
 	src="https://www.vedetta.org/vedetta/lavagna/index.html?nocache=1"
 	frameborder="0"
-	allowfullscreen="yes"
-	title="widget webcam"
+	allowfullscreen="true"
+	id="live-east"
+	title="widget webcam east"
 	class="mx-auto"
->
-</iframe>
+	scrolling="no"
+></iframe>
+
+<iframe
+	width="100%"
+	height="600"
+	src="https://vedetta.org/vedetta/cavi-di-lavagna2/index.php?nocache=1"
+	frameborder="0"
+	allowfullscreen="true"
+	id="live-west"
+	title="widget webcam west"
+	class="mx-auto"
+	scrolling="no"
+></iframe>

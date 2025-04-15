e0d57f34115d1914d0b65b64d09054eb529edfc4
Motocarota <motocarota@gmail.com>
2025-04-15 13:15:00 +0200

    adds meteo station iframe

--- src/routes/meteo/+page.svelte
+++ src/routes/meteo/+page.svelte
@@ -1,8 +1,6 @@
 <!-- LIMET - WIDGET PREVISIONI www.centrometeoligure.com - LIMET -->
 <iframe
 	src="https://www.centrometeoligure.com/widget/?comune=108&colore_sfondo=f1f1f1&colore_testo=2e4452&sfondo_comune=223442&testo_comune=ffffff"
-	width="400"
-	height="200"
 	frameBorder="0"
 	scrolling="no"
 	style="width:400px;height:200px;border:0;overflow:none;"
@@ -12,3 +10,13 @@
 	Widget LIMET non supportato - www.centrometeoligure.com
 </iframe>
 <!-- LIMET - Vietata la rimozione dei commenti www.dpsonline.it - LIMET -->
+<iframe
+	src="https://www.ecowitt.net/home/share?authorize=4TH7RY&device_id=cVhOU2lYWGVBMERpcFo4aXJUcVJSZz09"
+	frameBorder="0"
+	scrolling="no"
+	style="width:100vw;height:800px;border:0;min-width: 350px; max-width: 900px;"
+	class="mx-auto"
+	title="widget meteo"
+>
+	Widget meteo non supportato
+</iframe>

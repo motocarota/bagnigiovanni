2a018462b1f9dea9081ad49ecac2d7292aa611dc
Motocarota <motocarota@gmail.com>
2025-09-05 19:58:50 +0200

    fix meteo

--- src/routes/meteo/+page.svelte
+++ src/routes/meteo/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { compile } from 'svelte/compiler';
+</script>
+
 <!-- LIMET - WIDGET PREVISIONI www.centrometeoligure.com - LIMET -->
 <iframe
 	src="https://www.centrometeoligure.com/widget/?comune=108&colore_sfondo=f1f1f1&colore_testo=2e4452&sfondo_comune=223442&testo_comune=ffffff"
@@ -11,7 +15,7 @@
 </iframe>
 <!-- LIMET - Vietata la rimozione dei commenti www.dpsonline.it - LIMET -->
 <iframe
-	src="https://www.ecowitt.net/home/share?authorize=4TH7RY"
+	src="https://www.ecowitt.net/home/share?authorize=4AQ9M1"
 	frameBorder="0"
 	scrolling="no"
 	style="width:100vw;height:800px;border:0;min-width: 350px; max-width: 900px;"

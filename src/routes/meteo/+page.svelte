fbbf5f7fb610fdd9387d28ebac82e935e01203aa
Motocarota <motocarota@gmail.com>
2026-04-28 20:30:07 +0200

    fix meteo

--- src/routes/meteo/+page.svelte
+++ src/routes/meteo/+page.svelte
@@ -2,7 +2,6 @@
 	import { compile } from 'svelte/compiler';
 </script>
 
-<!-- LIMET - WIDGET PREVISIONI www.centrometeoligure.com - LIMET -->
 <iframe
 	src="https://www.centrometeoligure.com/widget/?comune=108&colore_sfondo=f1f1f1&colore_testo=2e4452&sfondo_comune=223442&testo_comune=ffffff"
 	frameBorder="0"
@@ -12,15 +11,4 @@
 	title="widget meteo"
 >
 	Widget LIMET non supportato - www.centrometeoligure.com
-</iframe>
-<!-- LIMET - Vietata la rimozione dei commenti www.dpsonline.it - LIMET -->
-<iframe
-	src="https://www.ecowitt.net/home/share?authorize=4AQ9M1"
-	frameBorder="0"
-	scrolling="no"
-	style="width:100vw;height:800px;border:0;min-width: 350px; max-width: 900px;"
-	class="mx-auto"
-	title="widget meteo"
->
-	Widget meteo non supportato
 </iframe>
